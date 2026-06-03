a68f21c2d3bba171db8fdbb7c7ed44c273444ebc
User <avivnissim100@gmail.com>
2026-06-04 00:49:35 +0300

    Configure FastAPI to serve templates and static files

--- app/main.py
+++ app/main.py
@@ -1,7 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 from app.database import init_db
 from app.database import init_db, get_connection
 from app.models import UserCreate, TradeCreate
+from app.database import init_db
+
+
 
 app = FastAPI()
 
