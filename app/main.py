8b4db6a97a8074e22d6fa21999f9e39b96a0a7fc
User <avivnissim100@gmail.com>
2026-06-04 04:05:51 +0300

    Fix RedirectResponse import for Render startup

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, Request, RedirectResponse
+from fastapi import FastAPI, Request
+from fastapi.responses import RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from app.database import init_db, get_connection
